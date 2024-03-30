6ab948b0d1248e7a3a60583483a0a82ba7cd6599
Matouš Kuchař <162641204+MatousKuchar01@users.noreply.github.com>
2024-03-30 16:11:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
+const imageContainer = document.getElementById("image-container");
+let photosArray = [];
+
+const count = 10;
+const apiKey = `xXdhtYr7KgrnonXXsdJe_X1NV8yGNW2T_eCUkcJ5pAo`;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function imageLoaded() {
+  console.log("image loaded");
+}
+
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+function displayPhotos() {
+  totalImages = photosArray.length;
+  console.log("total images", totalImages);
+  photosArray.forEach((photo) => {
+    const item = document.createElement("a");
+    //item.setAttribute("href", photo.links.html);
+    //item.setAttribute("target", "_blank");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+
+    const img = document.createElement("img");
+    //img.setAttribute("src", photo.urls.regular);
+    //img.setAttribute("alt", photo.alt_description);
+    //img.setAttribute("title", photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    img.addEventListener("load", imageLoaded);
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    console.log(photosArray);
+    displayPhotos();
+  } catch (error) {}
+}
+
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >=
+    document.body.offsetHeight - 1000
+  ) {
+    getPhotos();
+    console.log("load more");
+  }
+});
+
+getPhotos();
 
